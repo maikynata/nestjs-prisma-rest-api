b58913ad3658dcce3ab148077e3421bedd3303cd
maikynata <maikynata25@gmail.com>
2024-09-04 16:29:00 -0300

    change findall

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -12,9 +12,9 @@ export class RecipeService {
     return 'This action adds a new recipe';
   }
 
-  findAll() {
-    return `This action returns all recipe`;
-  }
+  async findAll() {
+    return this.prisma.recipe.findMany();
+}
 
   findOne(id: number) {
     return `This action returns a #${id} recipe`;
